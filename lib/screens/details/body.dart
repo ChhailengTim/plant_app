fd6e89a38edad15f6aa5a241319306c694fc5086
CHHAILENG TIM <80568234+ChhailengTim@users.noreply.github.com>
2022-08-07 13:56:23 +0700

    Update body.dart

--- lib/screens/details/body.dart
+++ lib/screens/details/body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:plant_app/models/btn_action.dart';
 import 'package:plant_app/models/image_and_icon.dart';
 import 'package:plant_app/models/title_and_price.dart';
 
@@ -29,43 +30,6 @@ class BodyDetail extends StatelessWidget {
               ],
             ),
           ],
-        ),
-      ),
-    );
-  }
-}
-
-class ButtonBuyNow extends StatelessWidget {
-  const ButtonBuyNow({
-    Key? key,
-    this.title,
-    this.textcolor,
-    this.color,
-  }) : super(key: key);
-  final String? title;
-  final Color? textcolor;
-  final Color? color;
-
-  @override
-  Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
-    return SizedBox(
-      width: size.width / 2,
-      height: 84,
-      child: Container(
-        decoration: BoxDecoration(
-          color: color ?? Colors.blue,
-          borderRadius: const BorderRadius.only(
-            topRight: Radius.circular(20),
-          ),
-        ),
-        child: Center(
-          child: Text(
-            title ?? "Buy Now",
-            style: Theme.of(context).textTheme.titleLarge!.copyWith(
-                  color: textcolor ?? Colors.white,
-                ),
-          ),
         ),
       ),
     );
